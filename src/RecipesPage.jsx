14830223a8ca40aa3f886b0b05edec0d96481a55
Leon Shimizu <lmshimizu@gmail.com>
2025-06-12 11:48:13 +1000

    Add axios and start implementing the handleindex

--- src/RecipesPage.jsx
+++ src/RecipesPage.jsx
@@ -1,38 +1,23 @@
+import axios from 'axios';
 import { RecipesIndex } from "./RecipesIndex";
 import { RecipesNew } from "./RecipesNew";
 
 export function RecipesPage() {
-  let recipes = [
-    {
-      id: 1,
-      title: "Raw Eggs",
-      chef: "Peter Jang",
-      image_url: "https://cdn.britannica.com/94/151894-050-F72A5317/Brown-eggs.jpg"
-    },
-    {
-      id: 2,
-      title: "Mud Pie",
-      chef: "Jay Wengrow",
-      image_url: "https://static.onecms.io/wp-content/uploads/sites/9/2017/12/mud-pie-XL-RECIPE2016.jpg"
-    },
-    {
-      id: 3,
-      title: "Butter Chicken",
-      chef: "Leon Shimizu",
-      image_url: "https://img.taste.com.au/qPAElV3h/taste/2013/07/easy-butter-chicken-image1-197844-1.jpg"
-    },
-    {
-      id: 4,
-      title: "Cheese Pizza",
-      chef: "Anastasia Shimizu",
-      image_url: "https://img.taste.com.au/qPAElV3h/taste/2013/07/easy-butter-chicken-image1-197844-1.jpg"
-    }
-  ];
+  let recipes = [];
+
+  const handleIndex = () => {
+    axios.get("http://localhost:3000/recipes.json")
+      .then((response) => {
+        console.log(response.data);
+        recipes = response.data;
+      })
+  }
 
   return (
     <main>
       <RecipesNew />
       <RecipesIndex recipes={recipes} />
+      <button onClick={handleIndex}>Load Recipes</button>
     </main>
   );
 }
