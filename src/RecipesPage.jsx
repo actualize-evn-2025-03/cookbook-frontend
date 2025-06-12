c74ca4d790ff9e085fc54ccd4ed6925c634fc2a8
Leon Shimizu <lmshimizu@gmail.com>
2025-06-13 09:26:43 +1000

    Added useState and useEffect to finish the handleIndex function

--- src/RecipesPage.jsx
+++ src/RecipesPage.jsx
@@ -1,23 +1,25 @@
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { RecipesIndex } from "./RecipesIndex";
 import { RecipesNew } from "./RecipesNew";
 
 export function RecipesPage() {
-  let recipes = [];
+  const [recipes, setRecipes] = useState([]);
 
   const handleIndex = () => {
     axios.get("http://localhost:3000/recipes.json")
       .then((response) => {
         console.log(response.data);
-        recipes = response.data;
+        setRecipes(response.data);
       })
   }
+
+  useEffect(handleIndex, [])
 
   return (
     <main>
       <RecipesNew />
       <RecipesIndex recipes={recipes} />
-      <button onClick={handleIndex}>Load Recipes</button>
     </main>
   );
 }
