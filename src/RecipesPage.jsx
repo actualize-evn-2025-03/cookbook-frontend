044f0b430efb69037c9c5199bd5090a6bba2ec00
Leon Shimizu <lmshimizu@gmail.com>
2025-06-13 10:40:22 +1000

    Implemented the RecipesShow and modal

--- src/RecipesPage.jsx
+++ src/RecipesPage.jsx
@@ -2,20 +2,25 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { RecipesIndex } from "./RecipesIndex";
 import { RecipesNew } from "./RecipesNew";
+import { Modal } from './Modal';
 
 export function RecipesPage() {
   const [recipes, setRecipes] = useState([]);
-
+  const [isRecipeShowVisible, setIsRecipeShowVisible] = useState(false); 
+  const [currentRecipe, setCurrentRecipe] = useState({}); 
+  
   const handleIndex = () => {
     axios.get("http://localhost:3000/recipes.json")
-      .then((response) => {
-        console.log(response.data);
-        setRecipes(response.data);
-      })
+    .then((response) => {
+      console.log(response.data);
+      setRecipes(response.data);
+    })
   }
-
+  
   const handleShow = (recipe) => {
     console.log("handleShow", recipe);
+    setIsRecipeShowVisible(true);
+    setCurrentRecipe(recipe);
   }
 
   useEffect(handleIndex, [])
@@ -24,6 +29,11 @@ export function RecipesPage() {
     <main>
       <RecipesNew />
       <RecipesIndex recipes={recipes} onShow={handleShow} />
+      <Modal show={isRecipeShowVisible} onClose={() => setIsRecipeShowVisible(false)} >
+        <h2>{currentRecipe.title}</h2>
+        <p>Chef: {currentRecipe.chef}</p>
+        <p>Directions: {currentRecipe.directions}</p>
+      </Modal>
     </main>
   );
 }
